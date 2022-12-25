8c0490e9ea4d213990f5078b6b5616a43cfaac03
maya-dav <mayaaktersaddam@gmail.com>
2022-12-25 23:39:11 +0600

    first

---
+++ js/script.js
@@ -0,0 +1,25 @@
+
+$('.teamslick').slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  dots: true,
+  arrows:false,
+  centerPadding:"0px",
+  centerMode: true,
+});
+
+
+var mixer = mixitup('.mixer');
+
+
+
+
+
+$('.testislide').slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  dots: true,
+  arrows:false,
+  centerPadding:"0px",
+  centerMode: true,
+});
